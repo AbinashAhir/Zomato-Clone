16015face8ec98e1e4b143cdc87a8b7b2673de86
Somnath Prasad <somnathprasad559@gmail.com>
2021-08-17 22:31:18 +0530

    reverse to carousel

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { GlobalStyles } from './global-style';
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <BrowserRouter>
-     <GlobalStyles />
     <App />
   </BrowserRouter>,
   rootElement
